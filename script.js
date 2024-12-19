c3b977e1642b438af160cd702493528991559278
lucyafersh <lucyafersh@gmail.com>
2024-12-20 01:51:15 +0200

    General editing

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 /* script.js */
 function toggleTheme() {
     document.documentElement.classList.toggle('light-theme');
+        // -------------------------------Dark-------------------------------
     if (document.documentElement.classList.contains('light-theme')) {
         document.documentElement.style.setProperty('--bg-color', '#f0f2f5');
         document.documentElement.style.setProperty('--text-color', '#333');
         document.documentElement.style.setProperty('--container-bg', 'white');
-        document.documentElement.style.setProperty('--link-color', '#007bff');
+        document.documentElement.style.setProperty('--link-color', '#00b7ff');
         document.documentElement.style.setProperty('--border-color', '#e0e0e0');
+        document.documentElement.style.setProperty('--button-color', '#000000');
     } else {
+        // -------------------------------Light-------------------------------
         document.documentElement.style.setProperty('--bg-color', '#121212');
         document.documentElement.style.setProperty('--text-color', '#f0f2f5');
-        document.documentElement.style.setProperty('--container-bg', '#1e1e1e');
-        document.documentElement.style.setProperty('--link-color', '#61dafb');
+        document.documentElement.style.setProperty('--container-bg', '#242424');
+        document.documentElement.style.setProperty('--link-color', '#00b7ff');
         document.documentElement.style.setProperty('--border-color', '#444');
+        document.documentElement.style.setProperty('--button-color', '#ffffff');
     }
 }
